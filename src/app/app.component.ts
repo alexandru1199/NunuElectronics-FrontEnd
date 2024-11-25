a22484db8f25974b5f665197dd5c300ae1326e26
Nieon <57Cristobis57@gmail.com>
2024-11-26 01:01:28 +0200

    brainrot_stage_0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,17 @@ import { TemplateComponent } from '../components/template/template.component';
 })
 export class AppComponent {
   title = 'Nunu-Electronics';
+  playAudio() {
+    let audio = new Audio();
+    audio.src = "../assets/sounds/ÜN ÜN ÜN.mp3"; // Ensure the path is correct
+    audio.load();
+    audio.play();
+    console.log(audio);
+
+    // Stop the audio after 2 seconds
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0; // Reset to the beginning if needed
+    }, 1300); // 2000ms = 2 seconds
+}
 }
