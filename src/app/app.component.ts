7950368a92e8937f347e78c73397069a0f9fc987
JustBlank-rgb <edocbadea@gmail.com>
2024-12-16 23:11:49 +0200

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateComponent } from '../components/template/template.component';
+import { ContactPageComponent } from '../components/contact-page/contact-page.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TemplateComponent],
+  imports: [RouterOutlet,TemplateComponent, ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
