2a5e7a3a76c8c6d6d17608706d2fae18ba746833
JustBlank-rgb <edocbadea@gmail.com>
2024-12-16 23:26:57 +0200

    yes2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TemplateComponent } from '../components/template/template.component';
+<<<<<<< HEAD
 import { HomepageComponent } from '../components/homepage/homepage.component';
 import { NavbarComponent } from "../components/navbar/navbar.component";
+=======
+import { ContactPageComponent } from '../components/contact-page/contact-page.component';
+>>>>>>> 7950368 (yes)
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< HEAD
   imports: [ TemplateComponent, HomepageComponent, NavbarComponent,RouterOutlet, RouterLink, RouterLinkActive],
+=======
+  imports: [RouterOutlet,TemplateComponent, ContactPageComponent],
+>>>>>>> 7950368 (yes)
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
