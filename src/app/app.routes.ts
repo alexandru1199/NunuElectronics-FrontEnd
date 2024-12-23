578c17c5f14bf818ed169435213b49ebd72ef2fd
Nieon <57Cristobis57@gmail.com>
2024-12-23 23:05:03 +0200

    brainrot_stage2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { TemplateComponent } from '../components/template/template.component';
 import { HomepageComponent } from '../components/homepage/homepage.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { ErrorPageComponent } from '../components/error-page/error-page.component';
 
 
 export const routes: Routes = [
     {path:'template',component:TemplateComponent},
     {path:'navbar',component:NavbarComponent},
     {path:'',component:HomepageComponent},
+    {path:'**',component:ErrorPageComponent},
     
 
 ];
