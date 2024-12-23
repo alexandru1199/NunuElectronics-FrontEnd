c785ab25d1a61c24ee1c89ebc505db9e9ea86ac8
Nieon <57Cristobis57@gmail.com>
2024-12-23 23:20:15 +0200

    brainrot_stage2.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { TemplateComponent } from '../components/template/template.component';
 import { HomepageComponent } from '../components/homepage/homepage.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { ErrorPageComponent } from '../components/error-page/error-page.component';
+import { ContactPageComponent } from '../components/contact-page/contact-page.component';
 
 
 export const routes: Routes = [
     {path:'template',component:TemplateComponent},
     {path:'navbar',component:NavbarComponent},
+    {path:'contact',component:ContactPageComponent},
     {path:'',component:HomepageComponent},
     {path:'**',component:ErrorPageComponent},
     
