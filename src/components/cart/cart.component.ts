16bf8bd589a9af7e8c6c5ab6a45286aa37617d7a
DESKTOP-9IJ4MU3\sirbu <sirbualexvladut3@yahoo.com>
2025-05-08 06:26:22 +0300

    yiipie cupon final ver

--- src/components/cart/cart.component.ts
+++ src/components/cart/cart.component.ts
@@ -21,8 +21,8 @@ export class CartComponent implements OnInit, OnDestroy {
   userId: number | null = null;  // User ID
   errorMessage: string | null = null; // Adaugă un câmp pentru mesajele de eroare
   couponApplied: boolean = false;  // Flag pentru a urmări dacă cuponul a fost aplicat
+  discountPercentage: number = 0;  // Procentajul discount-ului
   discountAmount: number = 0;  // Suma economisită
-
   constructor(
     private cartService: CartService,
     private couponService: CouponService,  // Injectează serviciul pentru cupoane
@@ -69,26 +69,21 @@ export class CartComponent implements OnInit, OnDestroy {
   onCheckout(): void {
     this.router.navigate(['/orderdetails']);
   }
-
-  // Metodă pentru aplicarea unui cupon
   applyCoupon(): void {
     if (this.userId && this.couponCode) {
       this.couponService.useCoupon(this.userId, this.couponCode).subscribe(
         (response) => {
           console.log('Coupon applied successfully:', response);
-          // Calculăm suma economisită, presupunând că răspunsul include un câmp 'discount'
-          this.discountAmount = response.discount || 0;
+          this.discountPercentage = response.discount || 0;
+          
+          const total = parseFloat(this.getTotal());
+          // Calculăm discount-ul ca un număr
+          this.discountAmount = (total * this.discountPercentage) / 100;
           alert('Cupon aplicat cu succes!');
-          this.couponApplied = true;  // Marchez cuponul ca aplicat
-  
-          // Actualizează totalul cu discount-ul aplicat
-          const total = this.getTotal();
-          const discountedTotal = parseFloat(total) - this.discountAmount;
-          console.log('Total cu discount aplicat: ', discountedTotal.toFixed(2));
+          this.couponApplied = true;
         },
         (error) => {  
           console.log('Error applying coupon:', error.error.error);
-          // Verifică dacă eroarea este legată de cuponul care nu a fost găsit sau deja utilizat
           if (error.error.error === 'Coupon not found or already used.') {
             alert('Cuponul nu a fost găsit sau a fost deja utilizat.');
           } else {
@@ -100,10 +95,14 @@ export class CartComponent implements OnInit, OnDestroy {
       alert('Completează toate câmpurile corect.');
     }
   }
+  
+  
+  
+
+  // Calculăm totalul cu discount aplicat pe bază de procentaj
   getTotalWithDiscount(): string {
     const total = parseFloat(this.getTotal());
-    const discountedTotal = total - this.discountAmount;
-    return discountedTotal.toFixed(2);
+    const discountedTotal = total - this.discountAmount;  // Folosim discountAmount ca un număr
+    return discountedTotal.toFixed(2);  // Afișăm rezultatul rotunjit la 2 zecimale
   }
-  
 }
